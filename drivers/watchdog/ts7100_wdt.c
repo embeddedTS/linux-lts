69f9ac46449439015795070a8b20ceaad0412ab0
Lionel D. Hummel <lionel@embeddedTS.com>
2023-09-21 14:26:15 -0700

    fixup! watchdog: ts7100_wdt: Added initial driver (back out that commit)

--- drivers/watchdog/ts7100_wdt.c
+++ drivers/watchdog/ts7100_wdt.c
@@ -1,15 +1,5 @@
-/*
- * i2c watchdog
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+// SPDX-License-Identifier: GPL-2.0+
+
 #include <linux/reboot.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -22,14 +12,16 @@
 #include <linux/slab.h>
 #include <asm/system_misc.h>
 
-/* NOTE:
- * We have observed that some large drives can have very large startup times.
- * A 5 minute default timeout means that, even with a very large and slow drive,
- * the system should eventually boot and userspace can actually start feeding.
- */
-#define TS_DEFAULT_TIMEOUT (60 * 5)
+#define TS_DEFAULT_TIMEOUT 30
 
-static struct i2c_client *client;
+static int wdt_timeout;
+module_param(wdt_timeout, int, 0);
+MODULE_PARM_DESC(wdt_timeout, "Watchdog timeout in seconds");
+
+static bool nowayout = WATCHDOG_NOWAYOUT;
+module_param(nowayout, bool, 0);
+MODULE_PARM_DESC(nowayout, "Watchdog cannot be stopped once started default="
+		__MODULE_STRING(WATCHDOG_NOWAYOUT) ")");
 
 /* This driver supports the embeddedTS 2nd generation WDT in
  * microcontroller.
@@ -66,16 +58,14 @@ static struct i2c_client *client;
 #define	WDT_FEED_CMD	0x01
 #define	WDT_SLEEP_CMD	0x02
 
-static int ts7100_wdt_write(u16 reg, u8 *data, int len)
+static int ts7100_wdt_write(struct i2c_client *client, u16 reg, u8 *data,
+			    int len)
 {
-	u8 *data_buf;
-	int ret;
 	struct i2c_msg msg;
+	u8 data_buf[6];
+	int ret;
 
-	data_buf = kzalloc(len+2, GFP_KERNEL);
-	if (!data_buf) {
-		return -ENOMEM;
-	}
+	BUG_ON(len > 4);
 
 	/* MSB of reg is written on the bus first */
 	data_buf[0] = ((reg >> 8) & 0xFF);
@@ -93,8 +83,6 @@ static int ts7100_wdt_write(u16 reg, u8 *data, int len)
 
 	ret = i2c_transfer(client->adapter, &msg, 1);
 
-	kfree(data_buf);
-
 	if (ret != 1) {
 		dev_err(&client->dev, "%s: write error, ret=%d\n",
 			__func__, ret);
@@ -106,68 +94,68 @@ static int ts7100_wdt_write(u16 reg, u8 *data, int len)
 
 /* This sets the timeout to <timeout> seconds. Does not issue a feed after. */
 static int ts7100_wdt_set_timeout(struct watchdog_device *wdt,
-				   unsigned int timeout)
+				  unsigned int timeout)
 {
+	struct i2c_client *client = to_i2c_client(wdt->parent);
 	u8 tmp[4];
 
-	dev_dbg(&client->dev, "%s\n", __func__);
-
-	if (watchdog_timeout_invalid(wdt, timeout)) {
-		return -EINVAL;
-	}
-
 	wdt->timeout = timeout;
-	/* WDT has centisecond granularity, kernel is easier with whole
-	 * second increments */
+	/*
+	 * WDT has centisecond granularity, kernel is easier with whole
+	 * second increments
+	 */
 	timeout *= 100;
 	tmp[0] = timeout & 0xFF;
 	tmp[1] = (timeout >> 8) & 0xFF;
 	tmp[2] = (timeout >> 16) & 0xFF;
 	tmp[3] = (timeout >> 24) & 0xFF;
 
-	return ts7100_wdt_write(WDT_TIMEOUT, tmp, 4);
+	return ts7100_wdt_write(client, WDT_TIMEOUT, tmp, 4);
 }
 
 static int ts7100_wdt_start(struct watchdog_device *wdt)
 {
+	struct i2c_client *client = to_i2c_client(wdt->parent);
 	u8 buf = WDT_FEED_CMD;
 
-	dev_dbg(&client->dev, "%s\n", __func__);
 	dev_dbg(&client->dev, "Feeding WDT with %d s\n", wdt->timeout);
 
-	return ts7100_wdt_write(WDT_CTRL, &buf, 1);
+	return ts7100_wdt_write(client, WDT_CTRL, &buf, 1);
 }
 
-static int ts_wdt_stop(struct watchdog_device *wdt)
+static int ts7100_wdt_stop(struct watchdog_device *wdt)
 {
+	struct i2c_client *client = to_i2c_client(wdt->parent);
 	u8 buf = WDT_FEED_CMD;
 
 	dev_dbg(&client->dev, "%s\n", __func__);
 
 	/* Feeding with a timeout of 0 will disable WDT */
 	ts7100_wdt_set_timeout(wdt, 0);
-	return ts7100_wdt_write(WDT_CTRL, &buf, 1);
+	return ts7100_wdt_write(client, WDT_CTRL, &buf, 1);
 }
 
 static int ts7100_wdt_restart(struct watchdog_device *wdt,
-  unsigned long a, void *b)
+			      unsigned long a, void *b)
 {
+	struct i2c_client *client = to_i2c_client(wdt->parent);
 	u8 buf = WDT_FEED_CMD;
 
 	dev_dbg(&client->dev, "%s\n", __func__);
 
 	/* Set WDT for 1 s timeout and stall CPU */
 	ts7100_wdt_set_timeout(wdt, 1);
-	ts7100_wdt_write(WDT_CTRL, &buf, 1);
+	ts7100_wdt_write(client, WDT_CTRL, &buf, 1);
 
-	while(1);
+	while (1)
+		;
 
 	return 0;
 }
 
-static struct watchdog_info ts7100_wdt_ident = {
+static struct watchdog_info ts7100_wdt_info = {
 	.options	= WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING |
-				WDIOF_MAGICCLOSE,
+			  WDIOF_MAGICCLOSE,
 	.identity	= "TS-7100 uC Watchdog",
 };
 
@@ -180,66 +168,53 @@ static struct watchdog_info ts7100_wdt_ident = {
 static struct watchdog_ops ts7100_wdt_ops = {
 	.owner		= THIS_MODULE,
 	.start		= ts7100_wdt_start,
-	.stop		= ts_wdt_stop,
+	.stop		= ts7100_wdt_stop,
 	.set_timeout	= ts7100_wdt_set_timeout,
 	.restart	= ts7100_wdt_restart,
-	/* .ioctl unneeded, watchdog core handles the common/necessary ioctls */
-
 };
 
 static struct watchdog_device ts_wdt_wdd = {
-	.info			= &ts7100_wdt_ident,
-	.ops			= &ts7100_wdt_ops,
-	/* A timeout of 0 means disable. Due to how the validity of the timeout
+	.info		= &ts7100_wdt_info,
+	.ops		= &ts7100_wdt_ops,
+	/*
+	 * A timeout of 0 means disable. Due to how the validity of the timeout
 	 * is checked, we need to spec a minimum timeout of 0. This allows the
 	 * FDT to spec a timeout of 0, disabling the WDT on startup. This may
 	 * cause issues in specific scenarios.
 	 */
-	.min_timeout		= 0,
-	.max_timeout		= 4294967,
-	.status			= WATCHDOG_NOWAYOUT_INIT_STATUS,
+	.min_timeout	= 0,
+	.max_timeout	= 4294967,
+	.status		= WATCHDOG_NOWAYOUT_INIT_STATUS,
+	.timeout	= TS_DEFAULT_TIMEOUT,
 };
 
-static int ts7100_wdt_probe(struct i2c_client *c,
+static int ts7100_wdt_probe(struct i2c_client *client,
 			  const struct i2c_device_id *id)
 {
-	int err;
-	u32 t;
-	client = c;
+	ts_wdt_wdd.parent = &client->dev;
 
-	dev_dbg(&client->dev, "%s\n", __func__);
-
-	/* TODO: Support power off events through the microcontroller
-	pm_power_off = do_ts_halt;
-	*/
-
-	watchdog_set_restart_priority(&ts_wdt_wdd, 255);
-
-	/* Attempt to read default value from FDT. If value is set, AND
-	 * it is the valid range, that value will be used. Otherwise, use
-	 * default kernel defined value.
+	/*
+	 * On supported platforms either:
+	 * The bootloader has already armed the WDT.
+	 * -or-
+	 * This platform's bootloader doesn't support starting the WDT and we
+	 * want it started as quickly as possible at boot.
+	 *
+	 * In either case, there is no mechanism to query the WDT running status
+	 * in hardware so we want to ensure the WDT is started and let the WDT
+	 * core know that it is. If CONFIG_WATCHDOG_HANDLE_BOOT_ENABLED is set,
+	 * then the WDT core will know to automatically feed this until
+	 * userspace can take over. If not set, a single feed takes place at
+	 * this point in time.we
 	 */
-	if (!of_property_read_u32(client->dev.of_node, "timeout-sec", &t) &&
-	  !watchdog_timeout_invalid(&ts_wdt_wdd, t)) {
-		ts_wdt_wdd.timeout = t;
-	} else {
-		dev_info(&client->dev,
-			"Unable to select timeout value, using default\n");
-		ts_wdt_wdd.timeout = TS_DEFAULT_TIMEOUT;
-	}
 
-	/* Set the timeout (most likely from DTS) and do a single feed until
-	 * userspace takes over finally.
-	 */
-	ts7100_wdt_set_timeout(&ts_wdt_wdd, ts_wdt_wdd.timeout);
-	ts7100_wdt_start(&ts_wdt_wdd);
+	if (of_property_read_bool(client->dev.of_node, "enable-early"))
+		set_bit(WDOG_HW_RUNNING, &ts_wdt_wdd.status);
 
-	err = watchdog_register_device(&ts_wdt_wdd);
-	if (err) {
-		return err;
-	}
+	watchdog_set_nowayout(&ts_wdt_wdd, nowayout);
+	watchdog_set_restart_priority(&ts_wdt_wdd, 128);
 
-	return 0;
+	return watchdog_register_device(&ts_wdt_wdd);
 }
 
 static const struct i2c_device_id ts7100_wdt_id[] = {
@@ -265,18 +240,7 @@ static struct i2c_driver ts7100_wdt_driver = {
 	.probe		= ts7100_wdt_probe,
 	.id_table	= ts7100_wdt_id,
 };
-
-static int __init ts_reboot_init(void)
-{
-	return i2c_add_driver(&ts7100_wdt_driver);
-}
-subsys_initcall(ts_reboot_init);
-
-static void __exit ts_reboot_exit(void)
-{
-	i2c_del_driver(&ts7100_wdt_driver);
-}
-module_exit(ts_reboot_exit);
+module_i2c_driver(ts7100_wdt_driver);
 
 MODULE_AUTHOR("Kris Bahnsen <kris@embeddedTS.com>");
 MODULE_DESCRIPTION("embeddedTS' TS-7100 (and compat.) WDT driver");
