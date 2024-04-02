4763c9176b6b35fb0887bef81d2c9379c2b016d0
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2024-04-02 22:50:29 +0300

    serial: max3100: Update uart_driver_registered on driver removal
    The removal of the last MAX3100 device triggers the removal of
    the driver. However, code doesn't update the respective global
    variable and after insmod — rmmod — insmod cycle the kernel
    oopses:
    
      max3100 spi-PRP0001:01: max3100_probe: adding port 0
      BUG: kernel NULL pointer dereference, address: 0000000000000408
      ...
      RIP: 0010:serial_core_register_port+0xa0/0x840
      ...
       max3100_probe+0x1b6/0x280 [max3100]
       spi_probe+0x8d/0xb0
    
    Update the actual state so next time UART driver will be registered
    again.
    
    Hugo also noticed, that the error path in the probe also affected
    by having the variable set, and not cleared. Instead of clearing it
    move the assignment after the successfull uart_register_driver() call.
    
    Fixes: 7831d56b0a35 ("tty: MAX3100")
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Hugo Villeneuve <hvilleneuve@dimonoff.com>
    Link: https://lore.kernel.org/r/20240402195306.269276-3-andriy.shevchenko@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/max3100-ts.c
+++ drivers/tty/serial/max3100-ts.c
@@ -898,7 +898,6 @@ static int max3100_probe(struct spi_device *spi)
 	mutex_lock(&max3100ts_common.max3100ts_lock);
 
 	if (!uart_driver_registered) {
-		uart_driver_registered = 1;
 		retval = uart_register_driver(&max3100_uart_driver);
 		if (retval) {
 			printk(KERN_ERR
@@ -906,6 +905,8 @@ static int max3100_probe(struct spi_device *spi)
 			mutex_unlock(&max3100ts_common.max3100ts_lock);
 			return retval;
 		}
+
+		uart_driver_registered = 1;
 	}
 
 	pdata = max3100_probe_dt(&spi->dev);
@@ -1027,6 +1028,7 @@ static void max3100_remove(struct spi_device *spi)
 
 	pr_debug("removing max3100 driver\n");
 	uart_unregister_driver(&max3100_uart_driver);
+	uart_driver_registered = 0;
 
 	mutex_unlock(&max3100ts_common.max3100ts_lock);
 }
