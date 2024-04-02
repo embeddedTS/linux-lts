7ff648e4b6bd5493884a4648a96d15f540956662
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2024-04-02 22:50:28 +0300

    serial: max3100: Lock port->lock when calling uart_handle_cts_change()
    XXX: THIS MAY NOT BE NEEDED AND MAY BREAK PORT LOCKING WE ADDED
    
    uart_handle_cts_change() has to be called with port lock taken,
    Since we run it in a separate work, the lock may not be taken at
    the time of running. Make sure that it's taken by explicitly doing
    that. Without it we got a splat:
    
      WARNING: CPU: 0 PID: 10 at drivers/tty/serial/serial_core.c:3491 uart_handle_cts_change+0xa6/0xb0
      ...
      Workqueue: max3100-0 max3100_work [max3100]
      RIP: 0010:uart_handle_cts_change+0xa6/0xb0
      ...
       max3100_handlerx+0xc5/0x110 [max3100]
       max3100_work+0x12a/0x340 [max3100]
    
    Fixes: 7831d56b0a35 ("tty: MAX3100")
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20240402195306.269276-2-andriy.shevchenko@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/max3100-ts.c
+++ drivers/tty/serial/max3100-ts.c
@@ -268,7 +268,7 @@ static int max3100_sr(struct max3100ts_port *s, u16 tx, u16 * rx)
 	return 0;
 }
 
-static int max3100_handlerx(struct max3100ts_port *s, u16 rx)
+static int max3100_handlerx_unlocked(struct max3100ts_port *s, u16 rx)
 {
 	unsigned int ch, flg, status = 0;
 	int ret = 0, cts;
@@ -312,6 +312,17 @@ static int max3100_handlerx(struct max3100ts_port *s, u16 rx)
 		spin_unlock_irqrestore(&s->port.lock, flags);
 	}
 
+	return ret;
+}
+
+static int max3100_handlerx(struct max3100ts_port *s, u16 rx)
+{
+	unsigned long flags;
+	int ret;
+
+	uart_port_lock_irqsave(&s->port, &flags);
+	ret = max3100_handlerx_unlocked(s, rx);
+	uart_port_unlock_irqrestore(&s->port, flags);
 	return ret;
 }
 
