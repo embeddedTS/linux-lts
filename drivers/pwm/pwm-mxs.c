1001653deb19b2464027fc70127056d01e8e3bee
Kris Bahnsen <kris@embeddedTS.com>
2025-07-18 00:26:32 +0000

    clk & pwm: fixups required for the TS-7680
    A combination of commits from 4.9-6.6 kernels to keep the 12 MHz PWM
    clock to the FPGA on after the kernel boots.
    
    pwm-clock is used as a clock source on the TS-7680, however the PWM and clk
    drivers all have some shortcomings that have been addressed in this patch.
    All of these changes are hidden behind matching OF machine compatible
    strings to the TS-7680.
    
    The PWM is reset, enabled, and set up in U-Boot; Linux just needs
    to let it keep running.
    
    - Invalid calculation of duty from DTS specified period. At 12 MHz, the
        math the kernel uses breaks down and cannot achieve 12 MHz.
    - Various steps of resetting the PWM block or clock gating. All of these
        result in a glitch on the PWM output from the CPU to the FPGA which
        can cause issues when later communicating with the FPGA.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- drivers/pwm/pwm-mxs.c
+++ drivers/pwm/pwm-mxs.c
@@ -114,7 +114,18 @@ static int mxs_pwm_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 			writel(1 << pwm->hwpwm, mxs->base + PWM_CTRL + SET);
 		}
 	} else {
-		clk_disable_unprepare(mxs->clk);
+		/*
+		 * If the PWM is not enabled, turn the clock off again to save power.
+		 */
+		/* XXX:
+		 * Do not ever let the clock get turned off on a TS-7680
+		 * The new atomic PWM API means that all PWMs start out as disabled
+		 * This is one of the contributing factors to the clock glitch that
+		 * occurs at startup. U-Boot sets everthing up properly in the case of
+		 * the TS-7680.
+		 */
+		if (!of_machine_is_compatible("fsl,imx28-ts7680"))
+			clk_disable_unprepare(mxs->clk);
 	}
 	return 0;
 }
