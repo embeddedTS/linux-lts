embeddedTS/linux-lts

commit c527b71f6fd8e60d5c2a7bfcc40e073d56d9f70c

ARM: embeddedts: rename tssupervisor subsystem to tswizard (#225)
* refactor(arch/arm/boot/dts/nxp/imx/): Rename supervisor to wizard
* refactor(arch/arm/configs/): Rename supervisor to wizard
* refactor(include/linux/mfd/): Rename supervisor to wizard
* refactor(drivers/mfd/): Rename sueprvisor to wizard
* refactor(drivers/power/reset/): Rename supervisor to wizard
* refactor(drivers/iio/temperature/): Rename supervisor to wizard
* refactor(drivers/iio/adc/): Rename supervisor to wizard
* refactor(drivers/rtc): Rename supervisor to wizard

drivers/iio/adc/tswizard_adc.c
@@ -13,16 +13,16 @@
 #include <linux/io.h>
 #include <linux/regmap.h>
 #include <linux/iio/iio.h>
-#include <linux/mfd/ts_supervisor.h>
+#include <linux/mfd/ts_wizard.h>
 
-#define TS_SUPERVISOR_MAX_ADC 32
+#define TS_WIZARD_MAX_ADC 32
 
 struct ts_adc {
-	struct ts_supervisor *super;
+	struct ts_wizard *wiz;
 	uint16_t channel_count;
 };
 
-#define SUPERVISOR_CHAN(index)					\
+#define WIZARD_CHAN(index)					\
 {								\
 	.type = IIO_VOLTAGE,					\
 	.indexed = 1,						\
@@ -38,39 +38,39 @@ struct ts_adc {
 	},							\
 }
 
-static const struct iio_chan_spec tssupervisor_channels[] = {
-	SUPERVISOR_CHAN(0),
-	SUPERVISOR_CHAN(1),
-	SUPERVISOR_CHAN(2),
-	SUPERVISOR_CHAN(3),
-	SUPERVISOR_CHAN(4),
-	SUPERVISOR_CHAN(5),
-	SUPERVISOR_CHAN(6),
-	SUPERVISOR_CHAN(7),
-	SUPERVISOR_CHAN(8),
-	SUPERVISOR_CHAN(9),
-	SUPERVISOR_CHAN(10),
-	SUPERVISOR_CHAN(11),
-	SUPERVISOR_CHAN(12),
-	SUPERVISOR_CHAN(13),
-	SUPERVISOR_CHAN(14),
-	SUPERVISOR_CHAN(15),
-	SUPERVISOR_CHAN(16),
-	SUPERVISOR_CHAN(17),
-	SUPERVISOR_CHAN(18),
-	SUPERVISOR_CHAN(19),
-	SUPERVISOR_CHAN(20),
-	SUPERVISOR_CHAN(21),
-	SUPERVISOR_CHAN(22),
-	SUPERVISOR_CHAN(23),
-	SUPERVISOR_CHAN(24),
-	SUPERVISOR_CHAN(25),
-	SUPERVISOR_CHAN(26),
-	SUPERVISOR_CHAN(27),
-	SUPERVISOR_CHAN(28),
-	SUPERVISOR_CHAN(29),
-	SUPERVISOR_CHAN(30),
-	SUPERVISOR_CHAN(31),
+static const struct iio_chan_spec tswizard_channels[] = {
+	WIZARD_CHAN(0),
+	WIZARD_CHAN(1),
+	WIZARD_CHAN(2),
+	WIZARD_CHAN(3),
+	WIZARD_CHAN(4),
+	WIZARD_CHAN(5),
+	WIZARD_CHAN(6),
+	WIZARD_CHAN(7),
+	WIZARD_CHAN(8),
+	WIZARD_CHAN(9),
+	WIZARD_CHAN(10),
+	WIZARD_CHAN(11),
+	WIZARD_CHAN(12),
+	WIZARD_CHAN(13),
+	WIZARD_CHAN(14),
+	WIZARD_CHAN(15),
+	WIZARD_CHAN(16),
+	WIZARD_CHAN(17),
+	WIZARD_CHAN(18),
+	WIZARD_CHAN(19),
+	WIZARD_CHAN(20),
+	WIZARD_CHAN(21),
+	WIZARD_CHAN(22),
+	WIZARD_CHAN(23),
+	WIZARD_CHAN(24),
+	WIZARD_CHAN(25),
+	WIZARD_CHAN(26),
+	WIZARD_CHAN(27),
+	WIZARD_CHAN(28),
+	WIZARD_CHAN(29),
+	WIZARD_CHAN(30),
+	WIZARD_CHAN(31),
 };
 
 static int ts_adc_iio_read_raw(struct iio_dev *iio_dev,
@@ -84,8 +84,8 @@ static int ts_adc_iio_read_raw(struct iio_dev *iio_dev,
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		addr = SUPER_ADC_BASE + chan->channel;
-		ret = regmap_read(adc->super->regmap, addr, &data);
+		addr = WIZ_ADC_BASE + chan->channel;
+		ret = regmap_read(adc->wiz->regmap, addr, &data);
 		if (ret < 0)
 			return ret;
 		*val = data;
@@ -104,22 +104,22 @@ static const struct iio_info ts_adc_info = {
 	.read_raw = &ts_adc_iio_read_raw,
 };
 
-static int ts_supervisor_adc_probe(struct platform_device *pdev)
+static int ts_wizard_adc_probe(struct platform_device *pdev)
 {
-	struct ts_supervisor *super = dev_get_drvdata(pdev->dev.parent);
+	struct ts_wizard *wiz = dev_get_drvdata(pdev->dev.parent);
 	struct ts_adc *adc;
 	struct device *dev = &pdev->dev;
 	struct iio_dev *indio_dev;
 	uint32_t chan_count;
 	int ret;
 
-	ret = regmap_read(super->regmap, SUPER_ADC_CHAN_ADV, &chan_count);
+	ret = regmap_read(wiz->regmap, WIZ_ADC_CHAN_ADV, &chan_count);
 	if (ret < 0) {
-		dev_err(dev, "error reading reg %u", SUPER_ADC_CHAN_ADV);
+		dev_err(dev, "error reading reg %u", WIZ_ADC_CHAN_ADV);
 		return ret;
 	}
 
-	/* This supervisor does not support ADC channels */
+	/* This wizard does not support ADC channels */
 	if (chan_count == 0)
 		return 0;
 
@@ -127,7 +127,7 @@ static int ts_supervisor_adc_probe(struct platform_device *pdev)
 	if (indio_dev == NULL)
 		return -ENOMEM;
 	adc = iio_priv(indio_dev);
-	adc->super = super;
+	adc->wiz = wiz;
 	adc->channel_count = chan_count;
 
 	/*
@@ -135,12 +135,12 @@ static int ts_supervisor_adc_probe(struct platform_device *pdev)
 	 * be up to 32 channels depending on muxes onboard and channels that
 	 * need to be sampled, but most will be < 7 channels.
 	 */
-	if (adc->channel_count > TS_SUPERVISOR_MAX_ADC) {
+	if (adc->channel_count > TS_WIZARD_MAX_ADC) {
 		dev_warn(dev, "The ADC device is advertising more ADC than supported!");
-		adc->channel_count = TS_SUPERVISOR_MAX_ADC;
+		adc->channel_count = TS_WIZARD_MAX_ADC;
 	}
 	indio_dev->num_channels = adc->channel_count;
-	indio_dev->channels = tssupervisor_channels;
+	indio_dev->channels = tswizard_channels;
 
 	indio_dev->name = dev_name(&pdev->dev);
 	indio_dev->dev.of_node = pdev->dev.of_node;
@@ -149,21 +149,21 @@ static int ts_supervisor_adc_probe(struct platform_device *pdev)
 	return devm_iio_device_register(&pdev->dev, indio_dev);
 }
 
-static const struct of_device_id tssupervisor_of_match[] = {
-	{ .compatible = "technologic,tssupervisor-adc", },
+static const struct of_device_id tswizard_of_match[] = {
+	{ .compatible = "technologic,tswizard-adc", },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, tsadc_of_match);
 
 static struct platform_driver tsadc_driver = {
 	.driver = {
-		.name   = "tssupervisor-adc",
-		.of_match_table = tssupervisor_of_match,
+		.name   = "tswizard-adc",
+		.of_match_table = tswizard_of_match,
 	},
-	.probe	= ts_supervisor_adc_probe,
+	.probe	= ts_wizard_adc_probe,
 };
 module_platform_driver(tsadc_driver);
 
-MODULE_DESCRIPTION("embeddedTS supervisor adc controller driver");
+MODULE_DESCRIPTION("embeddedTS wizard adc controller driver");
 MODULE_AUTHOR("Mark Featherston <mark@embeddedts.com>");
 MODULE_LICENSE("GPL");

arch/arm/boot/dts/nxp/imx/imx6ul-ts7180.dts
@@ -297,7 +297,7 @@
 		interrupts = <10 IRQ_TYPE_LEVEL_HIGH>;    /* See MX6UL_PAD_NAND_ALE__GPIO4_IO10 */
 	};
 
-	supervisor: wdt@54 {
+	wizard: wdt@54 {
 		compatible = "technologic,ts7100-wdt";
 		enable-early;
 		/* 5min timeout default, in case of slow userspace.

arch/arm/boot/dts/nxp/imx/imx6ul-ts7250v3.dts
@@ -8,11 +8,11 @@
 #include <dt-bindings/leds/common.h>
 #include "imx6ul-ts7250v3.dtsi"
 
-&supervisor {
+&wizard {
 	status = "okay";
 };
 
-&supervisor_rtc {
+&wizard_rtc {
 	status = "okay";
 };
 

arch/arm/boot/dts/nxp/imx/imx6ul-ts7250v3.dtsi
@@ -81,7 +81,7 @@
 
 	an_3p3v: an_3p3v {
 		compatible = "voltage-divider";
-		io-channels = <&supervisor_adc 0>;
+		io-channels = <&wizard_adc 0>;
 		label = "3.3V";
 		output-ohms = <100000>; /* R2 (100k) */
 		full-ohms = <110700>; /* R1 (10.7k) + R2 (100k) */
@@ -90,7 +90,7 @@
 
 	an_5v: an_5v {
 		compatible = "voltage-divider";
-		io-channels = <&supervisor_adc 3>;
+		io-channels = <&wizard_adc 3>;
 		label = "5V_A";
 		output-ohms = <42200>; /* R2 (42.2k) */
 		full-ohms = <84400>; /* R1 (42.2k) + R2 (42.2k) */
@@ -99,7 +99,7 @@
 
 	an_8v_48v: an_8v_48v {
 		compatible = "voltage-divider";
-		io-channels = <&supervisor_adc 4>;
+		io-channels = <&wizard_adc 4>;
 		label = "8V_48V";
 		output-ohms = <10700>; /* R2 (10.7k) */
 		full-ohms = <247700>; /* R1 (237K) + R2 (10.7k) */
@@ -111,18 +111,18 @@
 	 * that can be consumed within the kernel. The hwmon device is disabled
 	 * until this is resolved
 	 */
-	 supervisor-adcs {
+	 wizard-adcs {
 		compatible = "iio-hwmon";
-		io-channels = <&an_3p3v>, <&supervisor_adc 1>, <&supervisor_adc 2>,
-			<&supervisor_adc 3>, <&an_5v>, <&an_8v_48v>;
+		io-channels = <&an_3p3v>, <&wizard_adc 1>, <&wizard_adc 2>,
+			<&wizard_adc 3>, <&an_5v>, <&an_8v_48v>;
 		io-channel-names = "3.3V", "VDD_ARM_CAP", "VDD_SOC_CAP", "5V_A", "8V_48V";
 		status = "disabled";
 	};
 
-	supervisor-temp {
+	wizard-temp {
 		compatible = "iio-hwmon";
-		io-channels = <&supervisor_temp>;
-		io-channel-names = "Supervisor Temp";
+		io-channels = <&wizard_temp>;
+		io-channel-names = "Wizard Temp";
 		status = "okay";
 	};
 };
@@ -155,18 +155,18 @@
 		status = "disabled";
 	};
 
-	supervisor: supervisor@10 {
-		compatible = "technologic,supervisor";
+	wizard: wizard@10 {
+		compatible = "technologic,wizard";
 		reg = <0x10>;
 		status = "disabled";
 
-		supervisor_adc: supervisor_adc {
-			compatible = "technologic,supervisor-adc";
+		wizard_adc: wizard_adc {
+			compatible = "technologic,wizard-adc";
 			#io-channel-cells = <1>;
 		};
 
-		supervisor_temp: supervisor_temp {
-			compatible = "technologic,supervisor-temp";
+		wizard_temp: wizard_temp {
+			compatible = "technologic,wizard-temp";
 			#io-channel-cells = <0>;
 		};
 	};
@@ -177,8 +177,8 @@
 		status = "disabled";
 	};
 
-	supervisor_rtc: rtc@68 {
-		compatible = "technologic,supervisor-rtc";
+	wizard_rtc: rtc@68 {
+		compatible = "technologic,wizard-rtc";
 		reg = <0x68>;
 		wakeup-source;
 		status = "disabled";

arch/arm/configs/tsimx6ul_defconfig
@@ -667,7 +667,7 @@ CONFIG_GPIO_TS71XXWEIM=y
 CONFIG_GPIO_PCA953X=m
 CONFIG_GPIO_TS4900=y
 CONFIG_POWER_RESET=y
-CONFIG_POWER_RESET_TS_SUPERVISOR=y
+CONFIG_POWER_RESET_TS_WIZARD=y
 CONFIG_POWER_RESET_SYSCON=y
 CONFIG_POWER_RESET_SYSCON_POWEROFF=y
 CONFIG_SENSORS_IIO_HWMON=y
@@ -678,7 +678,7 @@ CONFIG_IMX_THERMAL=y
 CONFIG_WATCHDOG=y
 CONFIG_TS_WDT_MICRO=y
 CONFIG_IMX2_WDT=y
-CONFIG_MFD_TS_SUPERVISOR=y
+CONFIG_MFD_TS_WIZARD=y
 CONFIG_REGULATOR=y
 CONFIG_REGULATOR_FIXED_VOLTAGE=y
 CONFIG_REGULATOR_ANATOP=y
@@ -808,7 +808,7 @@ CONFIG_RTC_CLASS=y
 # CONFIG_RTC_NVMEM is not set
 CONFIG_RTC_INTF_DEV_UIE_EMUL=y
 CONFIG_RTC_DRV_DS1307=y
-CONFIG_RTC_DRV_TSSUPERVISOR=y
+CONFIG_RTC_DRV_TSWIZARD=y
 CONFIG_RTC_DRV_SNVS=y
 CONFIG_DMADEVICES=y
 CONFIG_IMX_SDMA=y
@@ -820,7 +820,7 @@ CONFIG_IMX_GPCV2_PM_DOMAINS=y
 CONFIG_IIO=y
 CONFIG_MMA8452=y
 CONFIG_TS_SIMPLEADC=y
-CONFIG_TS_SUPERVISOR_ADC=y
+CONFIG_TS_WIZARD_ADC=y
 CONFIG_VF610_ADC=y
 CONFIG_IIO_RESCALE=m
 CONFIG_INV_MPU6050_I2C=m
@@ -831,7 +831,7 @@ CONFIG_MAG3110=y
 CONFIG_IIO_ST_MAGN_3AXIS=m
 CONFIG_IIO_SYSFS_TRIGGER=m
 CONFIG_MPL3115=y
-CONFIG_TS_SUPERVISOR_TEMP=y
+CONFIG_TS_WIZARD_TEMP=y
 CONFIG_PWM=y
 CONFIG_PWM_IMX27=y
 CONFIG_PWM_TS=m

arch/arm/configs/tsimx6ul_minimal_defconfig
@@ -209,7 +209,7 @@ CONFIG_GPIO_TS71XXWEIM=y
 CONFIG_GPIO_PCA953X=m
 CONFIG_GPIO_TS4900=y
 CONFIG_POWER_RESET=y
-CONFIG_POWER_RESET_TS_SUPERVISOR=y
+CONFIG_POWER_RESET_TS_WIZARD=y
 CONFIG_POWER_RESET_SYSCON=y
 CONFIG_POWER_RESET_SYSCON_POWEROFF=y
 CONFIG_SENSORS_IIO_HWMON=y
@@ -220,7 +220,7 @@ CONFIG_IMX_THERMAL=y
 CONFIG_WATCHDOG=y
 CONFIG_TS_WDT_MICRO=y
 CONFIG_IMX2_WDT=y
-CONFIG_MFD_TS_SUPERVISOR=y
+CONFIG_MFD_TS_WIZARD=y
 CONFIG_REGULATOR=y
 CONFIG_REGULATOR_FIXED_VOLTAGE=y
 CONFIG_REGULATOR_ANATOP=y
@@ -281,7 +281,7 @@ CONFIG_RTC_CLASS=y
 # CONFIG_RTC_NVMEM is not set
 CONFIG_RTC_INTF_DEV_UIE_EMUL=y
 CONFIG_RTC_DRV_DS1307=y
-CONFIG_RTC_DRV_TSSUPERVISOR=y
+CONFIG_RTC_DRV_TSWIZARD=y
 CONFIG_RTC_DRV_SNVS=y
 CONFIG_DMADEVICES=y
 CONFIG_IMX_SDMA=y
@@ -293,7 +293,7 @@ CONFIG_IMX_GPCV2_PM_DOMAINS=y
 CONFIG_IIO=y
 CONFIG_MMA8452=y
 CONFIG_TS_SIMPLEADC=y
-CONFIG_TS_SUPERVISOR_ADC=y
+CONFIG_TS_WIZARD_ADC=y
 CONFIG_VF610_ADC=y
 CONFIG_IIO_RESCALE=m
 CONFIG_INV_MPU6050_I2C=m
@@ -304,7 +304,7 @@ CONFIG_MAG3110=y
 CONFIG_IIO_ST_MAGN_3AXIS=m
 CONFIG_IIO_SYSFS_TRIGGER=m
 CONFIG_MPL3115=y
-CONFIG_TS_SUPERVISOR_TEMP=y
+CONFIG_TS_WIZARD_TEMP=y
 CONFIG_PWM=y
 CONFIG_PWM_IMX27=y
 CONFIG_PWM_TS=m

drivers/iio/adc/Kconfig
@@ -1805,12 +1805,12 @@ config TS_SIMPLEADC
 	help
 	  Say yes here to build support for embeddedTS FPGA based ADC.
 
-config TS_SUPERVISOR_ADC
-	tristate "embeddedTS Supervisory Microcontroller ADC"
+config TS_WIZARD_ADC
+	tristate "embeddedTS Wizard Microcontroller ADC"
 	depends on OF
-	depends on MFD_TS_SUPERVISOR
+	depends on MFD_TS_WIZARD
 	help
-	  Say yes here to build support for embeddedTS supervisory
+	  Say yes here to build support for embeddedTS wizard
 	  microcontroller ADC.
 
 config TWL4030_MADC

drivers/iio/adc/Makefile
@@ -157,7 +157,7 @@ obj-$(CONFIG_TI_LMP92064) += ti-lmp92064.o
 obj-$(CONFIG_TI_TLC4541) += ti-tlc4541.o
 obj-$(CONFIG_TI_TSC2046) += ti-tsc2046.o
 obj-$(CONFIG_TS_SIMPLEADC) += ts_simple_adc.o
-obj-$(CONFIG_TS_SUPERVISOR_ADC) += tssupervisor_adc.o
+obj-$(CONFIG_TS_WIZARD_ADC) += tswizard_adc.o
 obj-$(CONFIG_TWL4030_MADC) += twl4030-madc.o
 obj-$(CONFIG_TWL6030_GPADC) += twl6030-gpadc.o
 obj-$(CONFIG_VF610_ADC) += vf610_adc.o

commit 730fd4f7a803eb3af52428c243c1ffaf327266bf

iio: adc: tswizard_adc: cleans up MODULE_DEVICE_TABLE
Fixes: c527b71f6fd8e ("ARM: embeddedts: rename tssupervisor subsystem to tswizard (#225)")
Fixes: fb3c60b03adab ("iio: tssupervisor-adc: Initial commit of embeddedTS supervisory microcontroller ADC")
Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

drivers/iio/adc/tswizard_adc.c
@@ -153,7 +153,7 @@ static const struct of_device_id tswizard_of_match[] = {
 	{ .compatible = "technologic,tswizard-adc", },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, tsadc_of_match);
+MODULE_DEVICE_TABLE(of, tswizard_of_match);
 
 static struct platform_driver tsadc_driver = {
 	.driver = {